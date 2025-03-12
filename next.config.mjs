3e83697cfad47a9b0805dda1398b477d1e23dcbd
Ihor Kazmin <ihor.kazmin@betterme.world>
2025-03-12 17:36:11 +0200

    prepare for gh pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+output: 'export',
+  basePath: '/nutrition', // Укажи название репозитория
+  images: {
+    unoptimized: true, // Отключает оптимизацию изображений (GitHub Pages не поддерживает API Next.js)
+  },
+};
 
 export default nextConfig;
